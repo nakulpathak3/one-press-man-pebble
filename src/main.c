8b81b9812ddb8f086e10b0f584e407a5bf57606a
Nakul Pathak <nakul.pathak@shopify.com>
2016-01-23 11:08:52 -0500

    Basic text and window working

---
+++ src/main.c
@@ -0,0 +1,36 @@
+#include <pebble.h>
+
+Window *window;
+TextLayer *text_layer;
+
+void window_load(Window* window) {
+  text_layer = text_layer_create(GRect(0, 0, 144, 168));
+  text_layer_set_background_color(text_layer, GColorClear);
+  text_layer_set_text_color(text_layer, GColorBlack);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+  text_layer_set_text(text_layer, "Triggered");
+}
+
+void window_unload(Window* window) {
+  
+}
+
+void init() {
+  window = window_create();
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+  window_stack_push(window, true);
+}
+
+void deinit() {
+  text_layer_destroy(text_layer);
+  window_destroy(window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
